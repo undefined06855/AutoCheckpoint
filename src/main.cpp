5da69599b5be57c69ddff5ac20ef551815f4d6f5
undefined06855 <98057141+undefined06855@users.noreply.github.com>
2024-03-05 20:29:09 +0000

    "fix" pauselayer btn

--- src/main.cpp
+++ src/main.cpp
@@ -128,6 +128,8 @@ class $modify(GJBaseGameLayer)
 };
 
 // enabled toggle!
+// this kinda breaks with betterpause because for some reason it forces the settings sprite scale to 0.6f
+// but i hope nobody will notice
 class $modify(MyPauseLayer, PauseLayer)
 {
 	void onAutoCheckpointSettings(CCObject* object)
@@ -135,40 +137,37 @@ class $modify(MyPauseLayer, PauseLayer)
 		geode::openSettingsPopup(Mod::get());
 	}
 	
+	// for some reason every single fucking time i add my button to ANY of the menus it BREAKS
+	// the layout so instead i'm going to hardcode the positions!!!!
 	void customSetup()
 	{
-		std::cout << "custmsetup" << std::endl;
 		PauseLayer::customSetup();
 
-		auto* lbm = this->getChildByID("left-button-menu");
-		if (!lbm)
-		{
-			// no left button menu this is so sad rip right button menu :(
+		if (!Mod::get()->getSettingValue<bool>("pause-layer-btn")) return;
 
-			std::cout << "no left button menu :(" << std::endl;
-			return;
-		}
+		auto winSize = CCDirector::sharedDirector()->getWinSize();
 
-		auto* spr = CCSprite::create("logo.png"_spr);
+		auto ccmenu = CCMenu::create();
+		ccmenu->setPosition(CCPoint{ 0, 0 });
+		ccmenu->setID("settings-button-menu"_spr);
+
+		auto* settingsButtonSprite = CCSprite::create("logo.png"_spr);
+		settingsButtonSprite->setScale(0.25f);
+
 		auto settingsBtn = CCMenuItemSpriteExtra::create(
-			spr,
+			settingsButtonSprite,
 			this,
 			menu_selector(MyPauseLayer::onAutoCheckpointSettings)
 		);
 		settingsBtn->setID("settings-button"_spr);
-		lbm->addChild(settingsBtn);
 
-
-		// this is so broken and will 100% break with other mods (only tested with click sounds) but oh well :)
-		if (auto rawLayout = lbm->getLayout()) {
-			if (auto layout = typeinfo_cast<AxisLayout*>(rawLayout)) {
-				layout->setCrossAxisOverflow(true);
-				layout->setAutoScale(false);
-			}
-		}
-
-		settingsBtn->setScale(0.3);
-		settingsBtn->m_baseScale = 0.3;
-		lbm->updateLayout();
+		// hardcoded positions!
+		auto padding = 20;
+		auto btnRadius = settingsBtn->getContentSize().height / 2;
+		auto offset = padding + btnRadius;
+		settingsBtn->setPosition(CCPoint{ winSize.width - offset, offset });
+		
+		ccmenu->addChild(settingsBtn);
+		this->addChild(ccmenu);
 	}
 };
